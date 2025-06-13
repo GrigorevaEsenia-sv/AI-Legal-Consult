6972185200e5b77dfd5895b6385ca636a367b4f4
GrigorevaEsenia-sv <grigorevaesenia388@gmail.com>
2025-06-13 18:37:06 +0300

    Delete src directory

--- src/etl/eval_bert_score.py
+++
@@ -1,6 +0,0 @@
-# Валидация через BERT score
-
-from bert_score import score
-def evaluate(predictions, references):
-    P, R, F1 = score(predictions, references, lang="ru")
-    return {"bert_precision": P.mean(), "bert_recall": R.mean(), "bert_f1": F1.mean()}
